11b050426dfae79cd17016219612641ff63c1754
Gustavo-l1528 <gustavo-lider1528@hotmail.com>
2022-11-15 18:16:07 -0300

    homework 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'nombre';
+const nuevaString = 'casa';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 15;
@@ -238,7 +238,7 @@ return 'Dato incorrecto';
 if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
 return 'Es vocal';
 }
-return 'Dato incorrecto'
+return 'Dato incorrecto';
 }
 
 
